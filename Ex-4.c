d1a24de4add9d075fee9e12bef1df0bc7a077c43
Krisha <krishameenatchi@gmail.com>
2026-04-08 22:04:48 +0530

    Create Ex-4.c

---
+++ Ex-4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n, bt[10], wt[10], tat[10], i, j, temp;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter Burst Times:\n");
+    for(i=0;i<n;i++) scanf("%d",&bt[i]);
+
+    // Sorting
+    for(i=0;i<n;i++) {
+        for(j=i+1;j<n;j++) {
+            if(bt[i] > bt[j]) {
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+            }
+        }
+    }
+
+    wt[0] = 0;
+    for(i=1;i<n;i++)
+        wt[i] = wt[i-1] + bt[i-1];
+
+    for(i=0;i<n;i++)
+        tat[i] = wt[i] + bt[i];
+
+    printf("\nBT\tWT\tTAT\n");
+    for(i=0;i<n;i++)
+        printf("%d\t%d\t%d\n", bt[i], wt[i], tat[i]);
+
+    return 0;
+}
