4779debc583ade7df97c7040658ed9e21bc9b2b5
Krisha <krishameenatchi@gmail.com>
2026-04-08 22:09:38 +0530

    Update Ex-6.c

--- Ex-6.c
+++ Ex-6.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-
 int main() {
     int n, bt[10], pr[10], rem[10], t=0, completed=0, i, highest;
-
     printf("Enter number of processes: ");
     scanf("%d",&n);
-
     printf("Enter Burst Time and Priority:\n");
     for(i=0;i<n;i++) {
         scanf("%d%d",&bt[i],&pr[i]);
         rem[i]=bt[i];
     }
-
     while(completed < n) {
         highest = -1;
 
@@ -20,14 +16,11 @@ int main() {
                 highest = i;
             }
         }
-
         rem[highest]--;
-        t++;
-
+        t++
         if(rem[highest]==0)
             completed++;
     }
-
     printf("Execution completed\n");
     return 0;
 }
