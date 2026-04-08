fd1e6b96acb751bade7b1e1873a229462e2e7a9e
Krisha <krishameenatchi@gmail.com>
2026-04-08 22:12:36 +0530

    Update Ex-2.c

--- Ex-2.c
+++ Ex-2.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 #include <fcntl.h>
-#include <unistd.h>
-
+#include <unistd.h
 int main() {
     int source, dest;
     char buffer[100];
     int n;
-
     source = open("source.txt", O_RDONLY);
     dest = open("dest.txt", O_WRONLY | O_CREAT, 0644);
-
     if (source < 0 || dest < 0) {
         printf("Error opening file\n");
         return 1;
     }
-
     while ((n = read(source, buffer, sizeof(buffer))) > 0) {
         write(dest, buffer, n);
     }
-
     close(source);
     close(dest);
-
     printf("File copied successfully\n");
     return 0;
 }
