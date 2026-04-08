a346608d64ac699c399464cffeeb497e54f3e4f1
Krisha <krishameenatchi@gmail.com>
2026-04-08 22:10:28 +0530

    Update Ex-5.c

--- Ex-5.c
+++ Ex-5.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 int main() {
     int n, bt[10], pr[10], wt[10], tat[10], i, j, temp;
-
     printf("Enter number of processes: ");
     scanf("%d", &n);
-
     printf("Enter Burst Time and Priority:\n");
     for(i=0;i<n;i++)
         scanf("%d %d",&bt[i], &pr[i]);
-
     // Sort by priority
     for(i=0;i<n;i++) {
         for(j=i+1;j<n;j++) {
@@ -19,17 +15,13 @@ int main() {
             }
         }
     }
-
     wt[0]=0;
     for(i=1;i<n;i++)
         wt[i]=wt[i-1]+bt[i-1];
-
     for(i=0;i<n;i++)
         tat[i]=wt[i]+bt[i];
-
     printf("\nBT\tPR\tWT\tTAT\n");
     for(i=0;i<n;i++)
         printf("%d\t%d\t%d\t%d\n", bt[i], pr[i], wt[i], tat[i]);
-
     return 0;
 }
